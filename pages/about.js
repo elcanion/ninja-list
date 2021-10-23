637e35c2d191d075771cbd584469b04ea0ce98ce
elcanion <beethovenssixth@gmail.com>
2021-10-23 20:19:35 -0300

    initial

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+        </>
+    );
+}
+ 
+export default About
